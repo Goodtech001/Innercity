ba8cf9c9b1efa4cb112c57c072cf41d39d42b2cc
goodtech001 <goodnewsnnebedim@gmail.com>
2025-08-14 00:18:53 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Body from "@/components/Body";
+import { Camp } from "@/components/Campaigns";
+import Category from "@/components/Category";
+import CornerTitle from "@/components/CornerTitle";
+import Gem from "@/components/Gem";
+import Hero from "@/components/Hero";
+import Par from "@/components/par";
+import { CarouselDemo } from "@/components/PartnersCorner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     <Hero />
+     <Body />
+     <Camp />
+     <Category/>
+     <Gem />
+     <CarouselDemo />
+     <Par />
+    </div>
+  );
+}
---
+++ components/CornerTitle.tsx
@@ -0,0 +1,13 @@
+
+
+function CornerTitle() {
+  return (
+    <div className="text-center">
+        <h1 className="text-4xl font-bold">
+            Partner's Corner
+        </h1>
+    </div>
+  )
+}
+
+export default CornerTitle
