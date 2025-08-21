88d53cc83e15dbc52881f22ddb2d9b1e4e8abae6
ryanfavour4 <ryanfave022@gmail.com>
2025-08-21 18:39:01 +0100

    `Removed CtaCard component and refactored CtaSection, PartnersCorners, and TopFundraisersVideo layouts to remove dependencies on it.`

--- layouts/top-fundraisers-video/index.tsx
+++ layouts/top-fundraisers-video/index.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { FreeMode, Navigation, Pagination } from 'swiper/modules'
 import Image from 'next/image'
 import thumbnail from '@/public/assets/images/video-thumbnail.jpg'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
 export default function TopFundraisersVideo() {
   const [isMobile, setIsMobile] = useState(false)
@@ -34,13 +35,6 @@ export default function TopFundraisersVideo() {
           <VideoPlayer />
         </div>
         <div className="">
-          <div className='relative'>
-           <div className="swiper-prev absolute top-1/2 w-10 border-2 border-gray-600 shadow bg-white p-2 rounded md:mt-10 mt-40">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-    </svg>
-  </div>
-  </div>
           <Swiper
             slidesPerView={isMobile ? 2 : 4}
             spaceBetween={5}
@@ -48,12 +42,22 @@ export default function TopFundraisersVideo() {
             pagination={{
               clickable: true,
             }}
-             navigation={{ prevEl: '.swiper-prev', nextEl: '.swiper-next' }}
+            navigation={{ prevEl: '.swiper-prev', nextEl: '.swiper-next' }}
             modules={[FreeMode, Pagination, Navigation]}
             className="relative min-h-40 max-w-4xl"
           >
-            <div className="absolute bottom-0 left-0 top-0 z-10 bg-gradient-to-tr from-white via-white/70 to-white/20 px-6 py-4" />
-            <div className="absolute bottom-0 right-0 top-0 z-10 bg-gradient-to-tl from-white via-white/70 to-white/20 px-6 py-4" />
+            <div className="absolute bottom-0 left-0 top-0 z-10 flex flex-col items-center justify-center bg-gradient-to-tr from-white via-white/70 to-white/20 px-6 py-4">
+              <button className="btn-white swiper-prev mb-10 px-1 py-1">
+                <Icon className="text-2xl" icon={'material-symbols-light:chevron-left-rounded'} />
+              </button>
+            </div>
+
+            <div className="absolute bottom-0 right-0 top-0 z-10 flex flex-col items-center justify-center bg-gradient-to-tl from-white via-white/70 to-white/20 px-6 py-4">
+              <button className="btn-white swiper-next mb-10 px-1 py-1">
+                <Icon className="text-2xl" icon={'material-symbols-light:chevron-right-rounded'} />
+              </button>
+            </div>
+
             {Array.from({ length: 8 }).map((_, index) => (
               <SwiperSlide key={index}>
                 <div className="">
@@ -68,26 +72,6 @@ export default function TopFundraisersVideo() {
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className='relative'>
-          <div className="
-    swiper-next
-    absolute
-    md:top-[-115px]
-    right-3
-    w-10
-    border-2
-    border-gray-600
-    shadow
-    bg-white
-    p-2
-    rounded
-
-  ">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-    </svg>
-  </div>
-  </div>
         </div>
       </div>
     </section>
