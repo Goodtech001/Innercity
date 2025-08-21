5f09d26ab05207c664aeec4ba977f089b780826c
goodtech001 <goodnewsnnebedim@gmail.com>
2025-08-21 15:59:42 +0100

    Thumbnail navigation arrow and cta change

--- layouts/cta-section/index.tsx
+++ layouts/cta-section/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import Ctaimg from '@/components/cta-image'
 import Ctacard from '@/components/cta-card'
 
 function CtaSection() {
   return (
-    <section className="relative">
-      <Ctaimg />
-      <div>
-        <Ctacard />
-      </div>
-    </section>
+    // <section className="relative bg-ctabg min-h-full bg-[100%,100%]">
+    //   {/* <Ctaimg /> */}
+    //   <div className='justify-self-center'>
+    //     <Ctacard />
+    //   </div>
+    // </section>
+    <section className="relative bg-ctabg min-h-full bg-[100%,100%] flex items-center justify-center">
+  <Ctacard />
+</section>
   )
 }
 
