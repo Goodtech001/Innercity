f61838cda57c7904b7c4f0586c578c030735dcb9
goodtech001 <goodnewsnnebedim@gmail.com>
2025-09-10 16:18:52 +0100

    extra

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -120,18 +120,22 @@ const ProfileSection = () => {
   const renderContent = () => {
     switch (activeTab) {
       case 'profile':
-        return <div>
-          <ImageUploader />
-          <DpUploader/>
-          <Editpage />
+        return (
+          <div>
+            <ImageUploader />
+            <DpUploader />
+            <Editpage />
           </div>
+        )
       case 'settings':
         return <div>Settings content</div>
       case 'notifications':
-        return <div>
-          <Mynotification />
-        </div>
-        case 'chat':
+        return (
+          <div>
+            <Mynotification />
+          </div>
+        )
+      case 'chat':
         return <div>Chat with Us.</div>
       default:
         return <div>Select a tab</div>
@@ -139,51 +143,47 @@ const ProfileSection = () => {
   }
 
   return (
-    <div className="${bgColorMap[activeTab]} md:flex h-screen transition-colors duration-500 p-2 gap-6 top">
-        {/* Content Area */}
-      <div className="md:w-2/3 h-full border-gray-100 border rounded overflow-y-auto p-2">
-        
+    <div className="${bgColorMap[activeTab]} top h-screen gap-6 p-2 transition-colors duration-500 md:flex">
+      {/* Content Area */}
+      <div className="h-full overflow-y-auto rounded border border-gray-100 p-2 md:w-2/3">
         {renderContent()}
       </div>
 
-
       {/* Sidebar */}
-     <div className="w-full md:w-1/3 bg-white py-2 border-gray-100 border rounded side flex md:block justify-between gap">
-     {/* <div className="md:w-1/3 overflow-y-hidden bg-white md:p-4 border-gray-100 border rounded side"> */}
+      <div className="flex rounded border border-gray-100 bg-white py-2 md:block md:w-1/3">
+        {/* <div className="md:w-1/3 overflow-y-hidden bg-white md:p-4 border-gray-100 border rounded side"> */}
 
         {/* <h2 className="mb-6 text-xl font-bold">My Profile</h2> */}
 
         <SidebarButton
           label="Profile"
           note="View & edit basic details about yourself"
-          icon={<Icon icon="mdi:user" width="24" height="24" className='icon'/>}
+          icon={<Icon icon="mdi:user" width="24" height="24" className="icon" />}
           onClick={() => setActiveTab('profile')}
           active={activeTab === 'profile'}
         />
         <SidebarButton
           label="Campaigns"
           note="Manage your campaigns here"
-          icon={<Icon icon="ri:funds-fill" width="24" height="24" className='icon'/>}
+          icon={<Icon icon="ri:funds-fill" width="24" height="24" className="icon" />}
           onClick={() => setActiveTab('settings')}
           active={activeTab === 'settings'}
         />
         <SidebarButton
           label="Notifications"
           note="See updates you should be aware of"
-          icon={<Icon icon="bxs:notification" width="24" height="24" className='icon'/>}
+          icon={<Icon icon="bxs:notification" width="24" height="24" className="icon" />}
           onClick={() => setActiveTab('notifications')}
           active={activeTab === 'notifications'}
         />
         <SidebarButton
           label="Chat"
           note="Chat with the ICM support team"
-          icon={<Icon icon="material-symbols:chat" width="24" height="24" className='icon'/>}
+          icon={<Icon icon="material-symbols:chat" width="24" height="24" className="icon" />}
           onClick={() => setActiveTab('chat')}
           active={activeTab === 'chat'}
         />
       </div>
-
-      
     </div>
   )
 }
@@ -199,14 +199,14 @@ type SidebarButtonProps = {
 const SidebarButton = ({ icon, label, onClick, active, note }: SidebarButtonProps) => (
   <button
     onClick={onClick}
-    className={`mb-2 md:flex w-full rounded md:px-4 px-2 md:py-3 transition md:hover:bg-gray-200`}
+    className={`md:mb-2 flex w-full flex-col items-center justify-center rounded transition md:flex-row md:px-4 md:py-3 md:hover:bg-gray-200`}
   >
-    <span className={`mr-4 btn-white w-fit p-2 ${
-      active ? 'btn-primary' : ''
-    }`}>{icon}</span>
-    <div className='text-sm text-left '>
-      <div className='text-black font-semibold md:text-base text-xs md:mt-0 mt-3 truncate lab'>{label}</div>
-      <div className='text-xs md:block hidden'>{note}</div>
+    <div className="flex items-center justify-center">
+      <span className={`btn-white mr-4 w-fit p-2 ${active ? 'btn-primary' : ''}`}>{icon}</span>
+    </div>
+    <div className="text-center text-sm md:text-left">
+      <div className="mt-3 text-xs font-semibold text-black md:mt-0 md:text-base">{label}</div>
+      <div className="hidden text-xs md:block">{note}</div>
     </div>
   </button>
 )
