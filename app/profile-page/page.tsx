94db1323cc7f2f76672c51019dd300e2e75c38fa
goodtech001 <goodnewsnnebedim@gmail.com>
2025-09-26 12:10:46 +0100

    CHANGES

--- app/profile-page/page.tsx
+++ app/profile-page/page.tsx
@@ -7,11 +7,6 @@ function page() {
   return (
     <div>
       <TopNavbar />
-      <div className="p-5">
-        <div className="border-color flex justify-between border-b">
-          <h1 className="mb-3 text-3xl font-bold text-black">Profile</h1>
-        </div>
-      </div>
       <ProfileSection />
       <Footer />
     </div>
