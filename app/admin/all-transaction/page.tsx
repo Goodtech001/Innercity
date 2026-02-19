4295afec17a32e0f6b70c9be7821fe8763fb3fc7
goodtech001 <goodnewsnnebedim@gmail.com>
2026-02-19 09:52:04 +0100

    init

--- app/admin/all-transaction/page.tsx
+++ app/admin/all-transaction/page.tsx
@@ -1,7 +1,7 @@
 import { columns } from '@/components/all-transaction/transaction-table'
 import { DataTable } from '@/components/all-transaction/data-table'
 import SearchInput from '@/components/search-input'
-import AdminSearchInput from '@/components/admin-search-input copy'
+// import AdminSearchInput from '@/components/admin-search-input copy'
 
 async function getStaff() {
   //   const res = await fetch("http://localhost:7070/api/admin/staff", {
@@ -20,7 +20,7 @@ export default async function StaffPage() {
     <div className="p-6">
       <div className="flex justify-between">
         <h1 className="mb-4 text-2xl font-bold">Transaction Management</h1>
-        <AdminSearchInput />
+        {/* <AdminSearchInput /> */}
       </div>
       <DataTable columns={columns} data={[]} />
     </div>
