c1fa6eb2455fd5d0164811212cbe0e0661c652e3
goodtech001 <goodnewsnnebedim@gmail.com>
2026-04-13 18:51:40 +0100

    Merge branch 'main' of https://github.com/Goodtech001/Innercity

---
+++ app/test-main/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function DetailsPage() {
+  return (
+    <div>
+      <h1>DETAILS PAGE</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum illo temporibus doloremque
+        atque quasi, minima amet quas, quae cum numquam reprehenderit eligendi ipsum reiciendis
+        deserunt modi voluptate. Est, sequi assumenda!
+      </p>
+    </div>
+  )
+}
---
+++ app/test-main/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>TEST PAGE</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum illo temporibus doloremque
+        atque quasi, minima amet quas, quae cum numquam reprehenderit eligendi ipsum reiciendis
+        deserunt modi voluptate. Est, sequi assumenda!
+      </p>
+    </div>
+  )
+}
