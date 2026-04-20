11b7df6b5316b4dcee49c6c1898d6a9cce52e631
goodtech001 <goodnewsnnebedim@gmail.com>
2026-04-20 01:51:49 +0100

    id

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://fundraise-api.onrender.com/api/v1'
 // export const baseUrl = 'http://localhost:3939/api'
-export const kingsChatClientId = 'baed362b-3960-482b-9e44-f61d72001668'
+export const kingsChatClientId = '62bdf551-b131-4ee6-aed4-5cac3340dacb'
