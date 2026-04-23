d18627b9b491f34ffad83d5c48508c6fbc61fd47
goodtech001 <goodnewsnnebedim@gmail.com>
2026-04-23 10:20:20 +0100

    env kingschat

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://fundraise-api.onrender.com/api/v1';
 // export const baseUrl = 'http://localhost:3939/api'
-export const kingsChatClientId = '62bdf551-b131-4ee6-aed4-5cac3340dacb'
+export const kingsChatClientId = process.env.NEXT_PUBLIC_KINGSCHAT_CLIENT_ID
